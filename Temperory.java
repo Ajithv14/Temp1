a14423ac572b05c47bfc4be994e0de14b40cfdc9
ajith <ajithvkanyakumari@gmail.com>
2024-08-30 10:00:49 +0530

    altered the print statement and inputs

---
+++ Temperory.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class Temperory{
+
+public static void main(String args[]){
+    int[][] matrix = new int [3][4];
+    matrix[1][0] = 5;
+    System.out.println(Arrays.deepToString(matrix));
+    
+}
+}
